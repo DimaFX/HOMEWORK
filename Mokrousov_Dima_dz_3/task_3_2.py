b82f90398b9600bc43a5411ff611ce18fd73acb9
DimaFX <dm.mokrousoff@yandex.ru>
2022-03-11 17:37:37 +0300

    Done

---
+++ Mokrousov_Dima_dz_3/task_3_2.py
@@ -0,0 +1,20 @@
+eng_rus_dict = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+def num_translate_adv(eng_word):
+    if eng_word[0].isupper():
+        eng_word = eng_word.lower()
+        return eng_rus_dict[eng_word].capitalize()
+    else:
+        return eng_rus_dict[eng_word]
+translation = input('Write a number from zero to ten by letters: ')
+print(num_translate_adv(translation))
